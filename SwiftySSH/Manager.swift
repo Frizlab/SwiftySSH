fd1ed13ebefe041eb988167e1508cace8e3fc7a6
Vlad Solomenchuk <vovasty@aramzamzam.net>
2016-08-26 11:13:14 -0700

    simpler Result implementation

--- SwiftySSH/Manager.swift
+++ SwiftySSH/Manager.swift
@@ -8,53 +8,6 @@
 
 import Foundation
 
-/// Used to represent whether a request was successful or encountered an error.
-///
-/// - success: The request and all post processing operations were successful resulting in the serialization of the
-///            provided associated value.
-///
-/// - failure: The request encountered an error resulting in a failure. The associated values are the original data
-///            provided by the server as well as the error that caused the failure.
-public enum Result<Value, Error: Swift.Error> {
-    case success(Value)
-    case failure(Error)
-    
-    /// Returns `true` if the result is a success, `false` otherwise.
-    public var isSuccess: Bool {
-        switch self {
-        case .success:
-            return true
-        case .failure:
-            return false
-        }
-    }
-    
-    /// Returns `true` if the result is a failure, `false` otherwise.
-    public var isFailure: Bool {
-        return !isSuccess
-    }
-    
-    /// Returns the associated value if the result is a success, `nil` otherwise.
-    public var value: Value? {
-        switch self {
-        case .success(let value):
-            return value
-        case .failure:
-            return nil
-        }
-    }
-    
-    /// Returns the associated error value if the result is a failure, `nil` otherwise.
-    public var error: Error? {
-        switch self {
-        case .success:
-            return nil
-        case .failure(let error):
-            return error
-        }
-    }
-}
-
 open class Manager {
     open let session: ManagedSession
 
@@ -173,7 +126,7 @@ open class Manager {
 }
 
 extension Manager {
-    public func request(_ host: String = "127.0.0.1", port: UInt16, send data: Data, receive: @escaping (Result<Data, NSError>) -> Void) {
+    public func request(_ host: String = "127.0.0.1", port: UInt16, send data: Data, receive: @escaping (Result<Data>) -> Void) {
         
         var resultData = Data()
         
